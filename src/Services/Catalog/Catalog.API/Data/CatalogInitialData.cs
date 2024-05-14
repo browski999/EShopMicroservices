6bd57fbdd7c885f8830a25df590fc61d84ff31fd
Rob Wright <bobazure1969@gmail.com>
2024-05-14 16:49:08 +0100

    Developed Catalog.API cross cutting concerns

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogInitialData.cs
@@ -0,0 +1,76 @@
+﻿
+namespace Catalog.API.Data
+{
+    public class CatalogInitialData : IInitialData
+    {
+        public async Task Populate(IDocumentStore store, CancellationToken cancellation)
+        {
+            using var session = store.LightweightSession();
+
+            if (await session.Query<Product>().AnyAsync())
+                return;
+
+            //MARTEN UPSERT will cater for existing records
+            session.Store<Product>(GetPreconfiguredProducts());
+            await session.SaveChangesAsync();
+        }
+
+        private IEnumerable<Product> GetPreconfiguredProducts() => new List<Product>()
+        {
+            new Product()
+            {
+                Id = new Guid("5334c996-8457-4cf0-815c-ed2b77c4ff61"),
+                Name = "IPhone X",
+                Description = "This phone is the company's biggest change to it's ",
+                ImageFile = "product-1.png",
+                Price = 950.00M,
+                Category = new List<string>{ "Smart Phone"}
+            },
+            new Product()
+            {
+                Id = new Guid("5334c996-8457-4cf0-815c-ed2b77c4ff62"),
+                Name = "Samsung 10",
+                Description = "This phone is the company's biggest change to it's ",
+                ImageFile = "product-2.png",
+                Price = 850.00M,
+                Category = new List<string>{ "Smart Phone"}
+            },
+            new Product()
+            {
+                Id = new Guid("5334c996-8457-4cf0-815c-ed2b77c4ff63"),
+                Name = "OnePlus 9t",
+                Description = "This phone is the company's biggest change to it's ",
+                ImageFile = "product-3.png",
+                Price = 800.00M,
+                Category = new List<string>{ "Smart Phone"}
+            },
+            new Product()
+            {
+                Id = new Guid("5334c996-8457-4cf0-815c-ed2b77c4ff64"),
+                Name = "Nokia 1A",
+                Description = "This phone is the company's biggest change to it's ",
+                ImageFile = "product-4.png",
+                Price = 150.00M,
+                Category = new List<string>{ "Smart Phone"}
+            },
+            new Product()
+            {
+                Id = new Guid("5334c996-8457-4cf0-815c-ed2b77c4ff65"),
+                Name = "Po",
+                Description = "This phone is the company's biggest change to it's ",
+                ImageFile = "product-5.png",
+                Price = 950.00M,
+                Category = new List<string>{ "Smart Phone"}
+            },
+            new Product()
+            {
+                Id = new Guid("5334c996-8457-4cf0-815c-ed2b77c4ff66"),
+                Name = "IPhone SE",
+                Description = "This phone is the company's biggest change to it's ",
+                ImageFile = "product-6.png",
+                Price = 950.00M,
+                Category = new List<string>{ "Smart Phone"}
+            }
+        };
+    }
+}
--- src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
@@ -1,23 +1,21 @@
 ﻿
 namespace Catalog.API.Products.GetProductById
 {
-    public record GetProductByIdQuery(Guid id) : IQuery<GetProductByIdResult>;
+    public record GetProductByIdQuery(Guid Id) : IQuery<GetProductByIdResult>;
 
     public record GetProductByIdResult(Product Product);
 
     internal class GetProductByIdQueryHandler
-        (IDocumentSession session, ILogger<GetProductByIdQueryHandler> logger)
+        (IDocumentSession session)
         : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
     {
         public async Task<GetProductByIdResult> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
         {
-            logger.LogInformation("GetProductByIdQueryHandler.Handle called with {@Query}", query);
-
-            var product = await session.LoadAsync<Product>(query.id, cancellationToken);
+            var product = await session.LoadAsync<Product>(query.Id, cancellationToken);
 
             if(product is null)
             {
-                throw new ProductNotFoundException(); 
+                throw new ProductNotFoundException(query.Id);
             }
 
             return new GetProductByIdResult(product);
