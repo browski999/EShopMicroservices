ff02e3609631e8dc5180c6900bcbc462e4265b5c
Rob Wright <bobazure1969@gmail.com>
2024-05-15 22:36:55 +0100

    Initial Basket implementation

---
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketHandler.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Basket.API.Basket.GetBasket
+{
+    public record GetBasketQuery(string username) : IQuery<GetBasketResult>;
+    public record GetBasketResult(ShoppingCart ShoppingCart);
+
+    public class GetBasketQueryHandler : IQueryHandler<GetBasketQuery, GetBasketResult>
+    {
+        public async Task<GetBasketResult> Handle(GetBasketQuery query, CancellationToken cancellationToken)
+        {
+            //TODO: get basket from the database
+            //var basket = await _repository.GetBasket(request.Username);
+
+            return new GetBasketResult(new ShoppingCart("swn"));
+        }
+    }
+}
